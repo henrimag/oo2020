bffc8c7dcfd2ceaa8bba9a7012eea316dcedffd5
henrimag <henrim@tlu.ee>
2020-02-04 11:16:47 +0200

    tund_1

--- w01_funktsioon/HelloWorld.java
+++ w01_funktsioon/HelloWorld.java
@@ -1,6 +1,24 @@
 public class HelloWorld {
+
     public static void main(String[] args) {
             System.out.println("Hello World!");
+            
+            byte minByte = Byte.MIN_VALUE;
+            byte maxByte = Byte.MAX_VALUE;
+            //byte, short, int, long, float, double, char, boolean
+
+            boolean flag = true;
+
+            System.out.println(i+1); // * / - %
+            int number = 5;
+            System.out.println(number++);
+            System.out.println(++number);
+
+            if (number < 5 && number > 0){
+                System.out.println("Jah");
+                }
+            for(int i = 0; i < 10; i++){
+                }
     }
 
 }
