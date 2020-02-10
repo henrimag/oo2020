1268c0640782a6e1e56ea856d74e5fbdd7a94230
Henri Mägi <55129119+henrimag@users.noreply.github.com>
2020-02-10 09:26:49 +0200

    Add files via upload

--- w01_funktsioon/HelloWorld.java
+++ w01_funktsioon/HelloWorld.java
@@ -9,16 +9,67 @@ public class HelloWorld {
 
             boolean flag = true;
 
-            System.out.println(i+1); // * / - %
+            System.out.println(1+1); // * / - %
             int number = 5;
             System.out.println(number++);
             System.out.println(++number);
 
-            if (number < 5 && number > 0){
+            if (number < 10 || number > 0){
                 System.out.println("Jah");
                 }
             for(int i = 0; i < 10; i++){
                 }
+
+            int counter = 0;
+            while (flag){
+                counter++;
+                if(counter == 10){
+                    System.out.println("I'm out");
+                    flag = false;
+                    }
+                }
+
+                int counterTwo = 0;
+                do {
+                    counterTwo++;
+                } while(counterTwo == 10);
+
+                int[] numberArray = new int[] {1, 2, 3, 4, 5};
+
+                for(int element : numberArray){
+                    System.out.println(element);
+                }
+                
+                for(int i = 0; i < numberArray.length; i++){
+                    System.out.println(numberArray[i]);
+                }
+                //String myName = "Henri";
+
+                //System.out.println(myName);
+
+                String name = name();
+
+                System.out.println(name);
+
+                HelloWorld helloWorld = new HelloWorld(); //konstruktori väljakutsumine
+                
+                //helloWorld.printSomething();
+
+                printSomething(10);
+                System.out.println(name());
+    }
+
+    static String name(){
+        return ""; // tühi string
+    }
+
+    static void printSomething(int number){
+        //int number;
+        for(int i = 1; i < 10; i++){
+            number += 1;
+        }
+        System.out.println(number);
+
     }
 
 }
