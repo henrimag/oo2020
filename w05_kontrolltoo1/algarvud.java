b85eddfa50cabb6281f55c9b0bd04ce96125eb64
Henri Mägi <55129119+henrimag@users.noreply.github.com>
2020-04-13 19:58:36 +0300

    Add files via upload

--- w05_kontrolltoo1/algarvud.java
+++ w05_kontrolltoo1/algarvud.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class algarvud {
 public static void main(String[] args) {
-Scanner s = new Scanner(System.in);
 
 System.out.println("Kontrollime kas sisestatud arv on algarv ja kas jagub 2, 3, 5 või 7-ga.");
 System.out.print("Sisesta arv: ");
@@ -47,10 +46,10 @@ public static void kasJagub(int num) {
             System.out.println("Sisestatud arv ei jagu 2, 3, 5 või 7-ga.");
     }
 }
-public static List<Integer> numberlist = new ArrayList<>();
+public static List<Integer> numbrilist = new ArrayList<>();
 public static List<Integer> lisaNumbrid() {
     int num = 0;
-    for (int i = 1; numberlist.size() != 1000; i++) {
+    for (int i = 1; numbrilist.size() != 1000; i++) {
         int counter = 0;
         for (num = i; num >= 1; num--) {
             if (i % num == 0) {
@@ -58,15 +57,15 @@ public static List<Integer> lisaNumbrid() {
             }
         }
         if (counter == 2) {
-            numberlist.add(i);
+            numbrilist.add(i);
         }
     }
-    return numberlist;
+    return numbrilist;
 }
 public static List<Integer> alguslist = new ArrayList<>();
 public static List<Integer> algarvRuut() {
     for (int i = 0; i < 10; i++) {
-        int value = numberlist.get(i);
+        int value = numbrilist.get(i);
         alguslist.add(value);
     }
     System.out.println("10 algarvu, mille ruudud on väiksemad, kui tuhandeda algarvu ruut: " + alguslist);
@@ -74,13 +73,13 @@ public static List<Integer> algarvRuut() {
 }
 public static List<Integer> l6pplist = new ArrayList<>();
 public static List<Integer> algarvL6pp() {
-        int length = numberlist.size();
-        int algus = length -10;
+        int length = numbrilist.size();
+        int algus = length -1;
         for(int i = algus; i < length; i++){
-            int value= numberlist.get(i);
+            int value= numbrilist.get(i);
             l6pplist.add(value);
         }
-        System.out.println("Kümme suurimat algarvu: " + l6pplist);
+        System.out.println("Suurim algarv on: " + l6pplist);
         return l6pplist;
     }
 }
