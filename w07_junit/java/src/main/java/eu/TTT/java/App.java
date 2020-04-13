9879afda7e0e2084a92ac1a7274168383f685ee9
Henry Pajuri <55189736+HenryPajuri@users.noreply.github.com>
2020-04-13 18:52:55 +0300

    Interface

--- w07_junit/java/src/main/java/eu/TTT/java/App.java
+++ w07_junit/java/src/main/java/eu/TTT/java/App.java
@@ -1,9 +1,8 @@
 package eu.TTT.java;
+
 import java.util.Scanner;
 
 interface TicTacToe {
-
-
 
     public String getName();
 
@@ -11,23 +10,16 @@ interface TicTacToe {
 
     void PrintBoard();
 
-   
-
 }
 
-class Game {
-
-}
 public class App implements TicTacToe {
 
-   
-    
     public String name;
     public static int row, column;
     public static char turn = 'X';
     public static Scanner scan = new Scanner(System.in);
     public static char[][] board = new char[3][3];
-    
+
     public static void main(String[] args) {
 
         TicTacToe game = new App();
@@ -87,7 +79,7 @@ public class App implements TicTacToe {
     public int maxValue(int a, int b) {
         return a > b ? a : b;
     }
-        
+
     public App(String name) {
         this.name = name;
     }
